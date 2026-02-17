fae56747f717d75333b4ac3de778a893a069e5f6
Pranjal <prai2702@gmail.com>
2026-02-17 20:56:47 +0900

    Final fix: ceo image change

--- src/components/sections/CeoVision.tsx
+++ src/components/sections/CeoVision.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import CeoImg from "@/components/images/ceo.jpg";
+import CeoImg from "@/components/images/ceo1.jpg";
 import Starry from "@/components/ui/Starry";
 
 export default function CeoVision() {
